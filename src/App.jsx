b4d386b45ada068d5f6dc37676410321a2e69aed
CroissantOfDestiny <radhesh2002@outlook.com>
2024-12-10 15:42:56 +1100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Greeting from "./components/Greeting"
+
+const App = () => {
+  return (
+    <>
+      <Greeting/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,11 @@
+const Greeting = () => {
+  return (
+    <section className="p-20">
+        <h1 className="font-upright shadowed text-8xl text-center text-white">
+            Welcome to the Whisper Meme Generator!
+        </h1>
+    </section>
+  )
+}
+
+export default Greeting
